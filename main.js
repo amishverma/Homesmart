5ebee4dcef07617c9477174ac9f7b10973e3b3f9
Amish Verma <amishverma1998@gmail.com>
2022-07-21 20:36:47 +0530

    Nabvar created but js not working

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import "./input.css"
+
+const navBtn=document.querySelector("#menu");
+const menuBar=document.querySelector("[role='menubar']")
+
+
+navBtn.addEventListener("click",()=>{
+    const isExpanded=JSON.parse(navBtn.getAttribute("aria-expanded"))
+
+    navBtn.setAttribute("aria-expanded",!isExpanded)
+    menuBar.classList.toggle("hiden")
+    menuBar.classList.toggle("flex")
+})
+
+console.log("hello")

af11b376e5a73b3d302aef4614010c992da46067
Amish Verma <amishverma1998@gmail.com>
2022-07-22 13:08:26 +0530

    Header section completed

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-import "./input.css"
+// import "./input.css"
 
 const navBtn=document.querySelector("#menu");
 const menuBar=document.querySelector("[role='menubar']")
 
 
 navBtn.addEventListener("click",()=>{
+    console.log("clicked")
     const isExpanded=JSON.parse(navBtn.getAttribute("aria-expanded"))
+    console.log(isExpanded)
 
     navBtn.setAttribute("aria-expanded",!isExpanded)
-    menuBar.classList.toggle("hiden")
+    menuBar.classList.toggle("hidden")
     menuBar.classList.toggle("flex")
 })
 

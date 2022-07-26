204e5da73adfa4dc90dfe53bc4fb1f6d3cd0773f
Amish Verma <amishverma1998@gmail.com>
2022-07-26 22:44:39 +0530

    Finished version 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.html","./*.js"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth:{
+        "16":"16rem",
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
